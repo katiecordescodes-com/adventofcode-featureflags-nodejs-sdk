a61f8059e8e8ec34f49b2f46113fbc1d6ccc1aeb
Katharine Cordes <gillis.katharine@gmail.com>
2023-02-22 22:57:54 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/__mocks__/**/*.ts',
+  ],
+  automock: false,
+  setupFiles: [
+    './setupJest.ts'
+  ]
+};
