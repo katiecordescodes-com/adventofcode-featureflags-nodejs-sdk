dbfc5c24b8b4588ddcacd621c62f121e96cac330
Katharine Cordes <gillis.katharine@gmail.com>
2023-02-26 22:38:26 -0700

    refactor: baseurl for service

--- src/FeatureFlagService.ts
+++ src/FeatureFlagService.ts
@@ -2,19 +2,11 @@
 import { FeatureFlagServiceInterface } from './FeatureFlagServiceInterface';
 
 export class FeatureFlagService implements FeatureFlagServiceInterface {
-  private readonly baseUrl: string;
-  private readonly protocol: string;
 
-  constructor(baseUrl: string = 'featureflags', isSecure: boolean = false) {
-    this.baseUrl = baseUrl;
-    this.protocol = 'http';
-    if (isSecure) {
-      this.protocol += 's';
-    }
-  }
+  constructor(private readonly baseUrl: string) {}
 
   async isFeatureFlagEnabled(id: string): Promise<boolean | null> {
-    const response = await fetch(`${this.protocol}://${this.baseUrl}/v1/featureflags/${id}/enabled`);
+    const response = await fetch(`${this.baseUrl}/v1/featureflags/${id}/enabled`);
     const body = await response.json();
 
     if (response.status === 404) {
